2a8bd3a1347e6995a94df3760ed82ae22b9dba30
Henrique Behr <65639781+HenriqueChianca@users.noreply.github.com>
2022-03-26 22:12:58 -0300

    Lab IMC
    Zip do exercício do lab 1 de orientação a objetos

---
+++ Labiratório 1/Labiratório 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Labiratório_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bob Nelson
+            Usuario usuarioBob = new Usuario("Bob Nelson", 37, 78, 1.70);
+            usuarioBob.CalcularIMC();
+            usuarioBob.Situacao();
+            usuarioBob.Meta();
+            Console.ReadLine();
+            Console.WriteLine("");
+
+            //Testolfo Rocha
+            Usuario usuarioTestolfo = new Usuario("Testolfo Rocha", 43, 60, 1.65);
+            usuarioTestolfo.CalcularIMC();
+            usuarioTestolfo.Situacao();
+            usuarioTestolfo.Meta();
+            Console.ReadLine();
+            Console.WriteLine("");
+
+            //Lisa Leite
+            Usuario usuarioLisa = new Usuario("Lisa Leite", 22, 92, 1.72);
+            usuarioLisa.CalcularIMC();
+            usuarioLisa.Situacao();
+            usuarioLisa.Meta();
+            Console.ReadLine();
+            Console.WriteLine("");
+
+        }
+    }
+}
---
+++ Labiratório 1/Labiratório 1/Usuario.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labiratório_1
+{
+    class Usuario
+    {
+        public String nome;
+        public int idade;
+        public double peso;
+        public double altura;
+        public double imc;
+        public Usuario(string nome, int idade, double peso, double altura)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        //Situação
+        public void Situacao()
+        {
+            
+            if (imc > 40)
+            {
+                Console.WriteLine("O usuário está com obesidade grau 3 e 4");
+            }
+
+            if (imc >= 35 && imc <= 40)
+            {
+                Console.WriteLine("O usuário está com obesidade grau 2");
+            }
+            if (imc >= 30 && imc <= 34.9)
+            {
+                Console.WriteLine("O usuário está com obesidade grau 1");
+            }
+            if (imc >= 25 && imc <= 29.9)
+            {
+                Console.WriteLine("O usuário está com sobrepeso");
+            }
+            if (imc >= 18.5 && imc <= 24.0)
+            {
+                Console.WriteLine("Parabéns, o usuário está em seu peso ideal");
+            }
+        }
+
+        //Método Calcular IMC
+        public void CalcularIMC()
+        {
+            //imc = peso / (altura) * altura;
+
+            double Alt = Math.Pow(altura,2);
+            double IMC1 = peso / Alt;
+            Console.WriteLine($"Seu IMC é: {IMC1}");
+            imc = IMC1;
+        }
+
+       //Meta
+       public void Meta()
+        {
+            if (imc > 24.9)
+            {
+                double IMCMeta = imc - 24.9d;
+                double Altura2 = Math.Pow(altura, 2);
+                double META = IMCMeta * Altura2;
+                Console.WriteLine($"É necessario perder: {META} Kg para o usuário chegar em seu peso ideal.");
+            }
+            else
+            {
+                Console.WriteLine("O usuário já está em seu peso ideal!");
+            }
+        }
+
+    }
+}

4d6b041bebadae8348b9a72f5908cbbb13a7469a
Henrique Behr <65639781+HenriqueChianca@users.noreply.github.com>
2022-03-31 10:05:32 -0300

    Lab1RevisadoExercício
    Exercicio de orientação a objeto que diz respeito a "Entrega- Laboratório 01 - Revisado"

---
+++ Lab1Versao2/Lab1Versao2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab1Versao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aperte enter para iniciar o programa!");
+
+            //Bob Nelson
+            Aluno usuarioBob = new Aluno("Bob Nelson", 37, 78, 1.70f);
+            Console.ReadLine();
+            Console.WriteLine($"{usuarioBob.nome} tem o IMC de {usuarioBob.CalcularIMC()},{usuarioBob.DescreverSituacaoCorporal()} e precisa perder {usuarioBob.CalcularMetaPerdaPeso()}kg");
+
+            //Testolfo Rocha
+            Aluno usuarioTestolfo = new Aluno("Testolfo Rocha", 43, 60, 1.65f);
+           
+            Console.ReadLine();
+            Console.WriteLine($"{usuarioTestolfo.nome} tem o IMC de {usuarioTestolfo.CalcularIMC()},{usuarioTestolfo.DescreverSituacaoCorporal()} e precisa perder {usuarioTestolfo.CalcularMetaPerdaPeso()}kg");
+
+            //Lisa Leite
+            Aluno usuarioLisa = new Aluno("Lisa Leite", 22, 92, 1.72f);
+           
+            Console.ReadLine();
+            Console.WriteLine($"{usuarioLisa.nome} tem o IMC de {usuarioLisa.CalcularIMC()},{usuarioLisa.DescreverSituacaoCorporal()} e precisa perder {usuarioLisa.CalcularMetaPerdaPeso()}kg");
+
+        }
+    }
+}

9ee31015c3f84014972acdb545a230966809ca0c
Henrique Behr <65639781+HenriqueChianca@users.noreply.github.com>
2022-04-11 11:45:13 -0300

    Lab2
    Exercicio do labaroatório 2 de orientação a objetos incompleto, tendo os métodos sacar e depositar feitos

---
+++ Lab2/Lab2/Lab2/Conta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Conta
+    {
+        private String idConta;
+        private String nomeCorrentista;
+        protected double saldoAtual;
+
+        public Conta(string idConta, string nomeCorrentista)
+        {
+            this.idConta = idConta;
+            this.nomeCorrentista = nomeCorrentista;
+            this.saldoAtual = 0;
+        }
+
+        protected string IdConta
+        {
+            get
+            {
+                return idConta;
+            }
+          
+        }
+
+        protected string NomeCorrentista
+        {
+            get
+            {
+                return nomeCorrentista;
+            }
+          
+        }
+
+        protected double SaldoAtual
+        {
+            get
+            {
+                return saldoAtual;
+            }
+
+            
+           
+        }
+
+        public virtual String SacarValor(double valor)
+        {
+            return "Operação não realizada";
+        }
+        public void DepositarValor(double valor)
+        {
+            saldoAtual = saldoAtual + valor;
+        }
+    }
+}
---
+++ Lab2/Lab2/Lab2/ContaPoupanca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class ContaPoupanca : Conta
+    {
+        const double tarifaSaque = 0.20 / 100;
+       public ContaPoupanca(string idConta, string nomeCorrentista):base(idConta, nomeCorrentista)
+        {
+        }
+        public override string SacarValor(double valor)
+        {
+            double valorTarifa = valor * tarifaSaque;
+            if (SaldoAtual - valor - valorTarifa < 0)
+            {
+                return "Operação não realizada. SALDO INSUFICIENTE!";
+            }
+            saldoAtual = SaldoAtual - valor - valorTarifa;
+            return "Operação realizada com sucesso!";
+        }
+    }
+}
